128d6e2264220689c0b3ce389364c3c0b0e87e99
Wojciech Ryrych <wojtek@ryrych.pl>
2011-02-24 11:44:30 +0100

    creating a dir structure

---
+++ widget/lib/jquery.ui.rlightbox.js
@@ -0,0 +1,30 @@
+/*
+ * jQuery UI rlightbox2
+ *
+ * Copyright 2011 Wojciech ‘rrh’ Ryrych
+ * licensed under the MIT license
+ *
+ * Depends:
+ *   jquery.ui.core.js
+ *   jquery.ui.widget.js
+ */
+(function( $, undefined ) {
+
+$.widget( "ui.progressbar", {
+	options: {
+	},
+
+	_create: function() {
+
+	},
+
+	destroy: function() {
+
+	},
+
+	_setOption: function( key, value ) {
+
+	},
+});
+
+})( jQuery );
