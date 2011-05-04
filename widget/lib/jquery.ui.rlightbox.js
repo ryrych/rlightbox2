b0114ff59e683e6acca0c2497794c37dbe89254c
Wojciech Ryrych <wojtek@ryrych.pl>
2011-05-04 16:13:19 +0200

    hide counter by default + small fix

--- widget/lib/jquery.ui.rlightbox.js
+++ widget/lib/jquery.ui.rlightbox.js
@@ -171,7 +171,7 @@ $.widget( "ui.rlightbox", {
 
 		$( "<div id='ui-lightbox' class='ui-widget ui-widget-content ui-corner-all' style='display: none'></div>" )
 			.append( "<div id='ui-lightbox-content' class='ui-widget-content'></div>" )
-			.append( "<div id='ui-lightbox-header' class='ui-widget-header ui-corner-all' style='display: none'><p id='ui-lightbox-header-wrapper'><span id='ui-lightbox-header-title'></span></p><p id='ui-lightbox-header-counter'><span id='ui-lightbox-header-counter-current'></span><span>z</span><span id='ui-lightbox-header-counter-total'></span><a id='ui-lightbox-header-close' href='#'><span class='ui-icon ui-icon-closethick'>close</span></a></p></div>" )
+			.append( "<div id='ui-lightbox-header' class='ui-widget-header ui-corner-all' style='display: none'><p id='ui-lightbox-header-wrapper'><span id='ui-lightbox-header-title'></span></p><p id='ui-lightbox-header-counter' style='display: none'><span id='ui-lightbox-header-counter-current'></span><span>z</span><span id='ui-lightbox-header-counter-total'></span></p><a id='ui-lightbox-header-close' href='#'><span class='ui-icon ui-icon-closethick'>close</span></a></div>" )
 			.appendTo( "body" )
 			.after( "<div id='ui-lightbox-overlay' class='ui-widget-overlay' style='display: none'></div>" );
 	},
